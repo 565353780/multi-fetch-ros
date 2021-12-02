bd6db5a96359fbb218413dce78195d30e52acd07
chLi <565353780@qq.com>
2021-12-02 19:51:15 +0800

    remove some unused files

--- fetch_gazebo/fetchit_challenge/scripts/spawn_assembly_delayed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Waiting for simulations to be ready"
-rosservice call /gazebo/get_physics_properties "{}"
-sleep 20
-
-echo "Spawning Assembly..."
-roslaunch fetchit_challenge assembly_spawn.launch &
