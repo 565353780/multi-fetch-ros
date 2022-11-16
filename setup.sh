c1b78641faded9dc0ea8c0c9d22bd552418a46a0
chLi <565353780@qq.com>
2022-11-16 21:01:16 +0800

    fix not installed via apt packages list

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
-sudo apt install ros-noetic-octomap libcgal-dev python3-catkin-tools
+sudo apt install ros-noetic-octomap libcgal-dev python3-catkin-tools \
+  ros-noetic-rotate-recovery ros-noetic-clear-costmap-recovery \
+  ros-noetic-nav-core ros-noetic-costmap-2d ros-noetic-move-base
 
